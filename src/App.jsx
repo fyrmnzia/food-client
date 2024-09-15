707038081cc4fc2bc0cda1bc6ea4b16020e462f4
fyrmnzia <1rifqi.firmansyah@gmail.com>
2024-09-16 00:25:04 +0700

    1st

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="bg-green font-primary">halo semuanya araranjing</h1>
+    </>
+  );
+}
+
+export default App;
